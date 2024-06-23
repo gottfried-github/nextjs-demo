8b94404c7f798124857b7d9d756d6e99a5d33632
gottfried <gottfried.dev@gmail.com>
2024-06-23 23:00:50 +0300

    add loading component

---
+++ src/app/components/Loading.jsx
@@ -0,0 +1,11 @@
+import IconLoading from './IconLoading'
+
+const Loading = () => {
+  return (
+    <div className="flex h-screen">
+      <IconLoading className="m-auto" />
+    </div>
+  )
+}
+
+export default Loading
